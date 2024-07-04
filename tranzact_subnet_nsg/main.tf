ff6183149291b1a65bc88f7164aaa737db350f2e
Alberto Caycho <parritocloud@outlook.com>
2024-07-04 00:29:33 -0500

    test update

--- tranzact_subnet_nsg/main.tf
+++ tranzact_subnet_nsg/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_subnet_network_security_group_association" "app3nsg" {
+resource "azurerm_subnet_network_security_group_association" "app3nsgattach" {
   subnet_id                 = var.subnet_id
   network_security_group_id = var.network_security_group_id
 }
