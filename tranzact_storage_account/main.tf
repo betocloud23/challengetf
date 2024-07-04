41e18f914d6495f62d47bcc4e999671604f78e1f
Alberto Caycho <parritocloud@outlook.com>
2024-07-04 00:42:22 -0500

    test creation

--- tranzact_storage_account/main.tf
+++ tranzact_storage_account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "app3stga" {
-  name                     = "${var.prefix}st499"
+  name                     = "${var.prefix}-sa"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
